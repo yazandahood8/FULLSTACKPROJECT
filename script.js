73667fce00a65754a3664a52ca1941f5f1587cd0
yazandahood <dahood.yazan8@gmail.com>
2025-04-06 09:53:52 +0300

    javascript updated

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-// פעולה 1: alert כשהדף נטען
 document.addEventListener("DOMContentLoaded", () => {
-  alert("Welcome");
+  alert("Welcome , website by Yazan and Maria");
 });
 
-// פעולה 2: שנה רקע בלחיצה
 function changeBackground() {
   document.body.style.backgroundColor = "#d1ecf1";
 }
 
-// צור כפתור דינמי
 const btn = document.createElement("button");
 btn.innerText = "btn";
 btn.className = "btn btn-primary mt-4";
 btn.onclick = changeBackground;
 
-// הוסף לכותרת
+
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".container").appendChild(btn);
 })
